b9b640851b63142760d845abd98f815c34309027
fkuhnert <fvk98@yahoo.com.br>
2017-06-21 23:55:50 -0300

    Initial commit

---
+++ NPC.h
@@ -0,0 +1,17 @@
+#ifndef NPC_H
+#define NPC_H
+
+typedef struct _NPC {
+    int posX;
+    int posY;
+    int stepX;
+    int stepY;
+    SDL_Surface* image;
+    int imgW;
+    int imgH;
+} NPC;
+
+extern NPC ball;
+extern NPC bars[30];
+
+#endif
